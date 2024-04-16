34df932bf998019be83009c43000bea20685b2a3
Jake Godsall <jake.edward.godsall@gmail.com>
2024-04-16 15:55:24 +0100

    added Hero section with text content and Socials bar

---
+++ frontend/src/app/(home)/components/Hero.jsx
@@ -0,0 +1,18 @@
+import SocialsBar from "@/app/components/SocialsBar";
+
+export default function Hero() {
+  return (
+    <section className="flex flex-col px-[1.6rem]">
+      <div className="flex flex-col mb-[6rem]">
+        <h1 className="text-[3.4rem] font-semibold mb-[0.8rem]">
+          Hi, I&apos;m Jake
+        </h1>
+        <p className="text-[1.6rem] leading-[2.4rem]">
+          I&apos;m a full-stack developer passionate about engineering
+          innovative tools to improve personal productivity.
+        </p>
+      </div>
+      <SocialsBar />
+    </section>
+  );
+}

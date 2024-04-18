e872545e60812b54f6648dc4fbccf487b3653172
Jake Godsall <jake.edward.godsall@gmail.com>
2024-04-18 09:01:17 +0100

    added grouping of daily logs into today, yesterday and last 7 days

--- frontend/src/app/daily-log/components/LogItem.jsx
+++ frontend/src/app/daily-log/components/LogItem.jsx
@@ -1,6 +1,46 @@
 import Image from "next/image";
 
-export default function LogItem({ id, datetime, project, content, skills }) {
+export default function LogItem({
+  id,
+  datetime,
+  project,
+  content,
+  skills,
+  withDate,
+}) {
+  function getOrdinalSuffix(day) {
+    if (day > 3 && day < 21) return "th"; // covers 4th to 20th
+    switch (day % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  }
+
+  function mapMonthNumberToStringRepresentation(month) {
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    return monthNames[month];
+  }
+
   function getTimeFromDateTime(datetime) {
     const obj = new Date(datetime);
     const hours = String(obj.getHours()).padStart(2, "0");
@@ -9,14 +49,28 @@ export default function LogItem({ id, datetime, project, content, skills }) {
     return `${hours}:${minutes}`;
   }
 
+  function getDateTimeFromDateTime(datetime) {
+    const obj = new Date(datetime);
+    const date = obj.getDate();
+    const month = obj.getMonth();
+    const hours = String(obj.getHours()).padStart(2, "0");
+    const minutes = String(obj.getMinutes()).padStart(2, "0");
+
+    return `${date}${getOrdinalSuffix(
+      date
+    )} ${mapMonthNumberToStringRepresentation(month)} ${hours}:${minutes}`;
+  }
+
   return (
     <div>
       <div className="flex items-center">
         <p className="mr-[1.6rem] italic text-[#6b6b6b] text-[1.2rem]">
-          {getTimeFromDateTime(datetime)}
+          {withDate
+            ? getDateTimeFromDateTime(datetime)
+            : getTimeFromDateTime(datetime)}
         </p>
         <p className="font-bold text-[1.2rem]">{project}</p>
-        <ul>
+        <ul className="ml-auto flex items-center gap-[1rem]">
           {skills.map((skill, idx) => (
             <li key={idx}>
               <Image
--- frontend/src/app/daily-log/page.jsx
+++ frontend/src/app/daily-log/page.jsx
@@ -4,69 +4,6 @@ import { useState, useEffect } from "react";
 
 import LogItem from "./components/LogItem";
 
-const DUMMY_LOGS = [
-  {
-    id: 1,
-    createdDate: "2024-04-08T10:32:00",
-    project: "Personal Portfolio",
-    content:
-      "Implemented a responsive navbar using Flexbox and JavaScript for improved mobile user experience.",
-    skills: [
-      { id: 1, name: "JavaScript", icon: "/javascript.svg" },
-      { id: 2, name: "React", icon: "/react.svg" },
-      { id: 3, name: "TailwindCSS", icon: "/tailwind.svg" },
-    ],
-  },
-  {
-    id: 2,
-    createdDate: "2024-04-08T12:45:00",
-    project: "E-commerce Site",
-    content:
-      "Integrated Stripe payment gateway for processing online payments securely.",
-    skills: [
-      { id: 4, name: "Stripe", icon: "/stripe.svg" },
-      { id: 2, name: "React", icon: "/react.svg" },
-      { id: 5, name: "Node.js", icon: "/nodejs.svg" },
-    ],
-  },
-  {
-    id: 3,
-    createdDate: "2024-04-08T14:30:00",
-    project: "Blog Platform",
-    content:
-      "Developed a custom rich text editor to enhance content creation experience.",
-    skills: [
-      { id: 1, name: "JavaScript", icon: "/javascript.svg" },
-      { id: 6, name: "Quill", icon: "/quill.svg" },
-      { id: 3, name: "TailwindCSS", icon: "/tailwind.svg" },
-    ],
-  },
-  {
-    id: 4,
-    createdDate: "2024-04-08T16:00:00",
-    project: "Health Tracker App",
-    content:
-      "Added a feature for users to track water intake and receive reminders.",
-    skills: [
-      { id: 2, name: "React", icon: "/react.svg" },
-      { id: 7, name: "Redux", icon: "/redux.svg" },
-      { id: 3, name: "TailwindCSS", icon: "/tailwind.svg" },
-    ],
-  },
-  {
-    id: 5,
-    createdDate: "2024-04-08T18:20:00",
-    project: "AI Personal Assistant",
-    content:
-      "Implemented voice recognition feature to interpret and respond to user commands.",
-    skills: [
-      { id: 8, name: "Python", icon: "/python.svg" },
-      { id: 9, name: "TensorFlow", icon: "/tensorflow.svg" },
-      { id: 10, name: "Keras", icon: "/keras.svg" },
-    ],
-  },
-];
-
 export default function DailyLogPage() {
   const [logData, setLogData] = useState([]);
 
@@ -85,19 +22,68 @@ export default function DailyLogPage() {
     fetchData();
   }, []);
 
-  console.log(logData);
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const yesterday = new Date(today.getTime() - 86400000);
+  const sevenDaysAgo = new Date(today.getTime() - 864000000 * 7);
+
+  const todayData = logData.filter((item) => {
+    const itemDate = new Date(item.datetime);
+    itemDate.setHours(0, 0, 0, 0);
+    return itemDate.getTime() === today.getTime();
+  });
+
+  const yesterdayData = logData.filter((item) => {
+    const itemDate = new Date(item.datetime);
+    itemDate.setHours(0, 0, 0, 0);
+    return itemDate.getTime() === yesterday.getTime();
+  });
+
+  const last7DaysData = logData.filter((item) => {
+    const itemDate = new Date(item.datetime);
+    itemDate.setHours(0, 0, 0, 0);
+    return itemDate >= sevenDaysAgo && itemDate < yesterday;
+  });
 
   return (
     <main className="px-[1.6rem] md:px-[3.5rem] pt-[3.5rem]">
       <h1 className="text-[3.2rem] font-semibold mb-[3rem]">Daily Log</h1>
-      <ul className="flex flex-col gap-[0.8rem]">
-        {logData.map((log) => (
+      <h2 className="text-[2.2rem] font-semibold mb-[1.6rem]">Today</h2>
+      <ul className="flex flex-col mb-[2rem] gap-[0.8rem]">
+        {todayData.map((log) => (
           <li key={log.id}>
             <LogItem
               project={log.project}
               datetime={log.datetime}
               content={log.content}
               skills={log.skills}
+            />
+          </li>
+        ))}
+      </ul>
+      <h2 className="text-[2.2rem] font-semibold mb-[1.6rem]">Yesterday</h2>
+      <ul className="flex flex-col mb-[2rem] gap-[0.8rem]">
+        {yesterdayData.map((log) => (
+          <li key={log.id}>
+            <LogItem
+              project={log.project}
+              datetime={log.datetime}
+              content={log.content}
+              skills={log.skills}
+            />
+          </li>
+        ))}
+      </ul>
+      <h2 className="text-[2.2rem] font-semibold mb-[1.6rem]">Last 7 days</h2>
+      <ul className="flex flex-col mb-[2rem] gap-[0.8rem]">
+        {last7DaysData.map((log) => (
+          <li key={log.id}>
+            <LogItem
+              project={log.project}
+              datetime={log.datetime}
+              content={log.content}
+              skills={log.skills}
+              withDate={true}
             />
           </li>
         ))}

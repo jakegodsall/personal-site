435ead3e152979053338403cab00e801b445dbbe
Jake Godsall <jake.edward.godsall@gmail.com>
2024-04-18 09:47:20 +0100

    added AboutMe section with lorem text

---
+++ frontend/src/app/(home)/components/AboutMe.jsx
@@ -0,0 +1,20 @@
+export default function AboutMe() {
+  return (
+    <section>
+      <h2 className="text-[2.4rem] font-semibold mb-[2.4rem]">About Me</h2>
+      <p className="text-[1.6rem] leading-[2.6rem] text-gray-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+        tempore iste suscipit amet perspiciatis quam consequuntur nostrum ea
+        aspernatur. Nam officia quae ex quas recusandae dolorum quo odit labore
+        beatae vitae accusantium vero ipsum ea error, reprehenderit, facere enim
+        aut molestiae tempora eligendi quibusdam sapiente temporibus. Minus
+        error excepturi a cumque id, cum quam? Eius corporis nostrum, velit
+        soluta incidunt fugit tenetur modi at porro nam dolorem quae omnis ex
+        maiores, repellendus odio. Tempore, sed expedita. Amet molestiae
+        repellendus dolor? Magnam quos possimus veniam deserunt animi totam
+        dolore eius laudantium quisquam? Veritatis accusamus labore ratione
+        provident distinctio, sed repudiandae perspiciatis!
+      </p>
+    </section>
+  );
+}
--- frontend/src/app/(home)/page.jsx
+++ frontend/src/app/(home)/page.jsx
@@ -1,9 +1,15 @@
+import AboutMe from "./components/AboutMe";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
+      <div className="px-[1.6rem] py-[6.4rem]">
+        <Hero />
+      </div>
+      <div className="px-[1.6rem] bg-grey-primary py-[6.4rem]">
+        <AboutMe />
+      </div>
     </main>
   );
 }

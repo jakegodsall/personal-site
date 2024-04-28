3f5909587ae24a5794c4ff656d8efdc49d80b61c
Jake Godsall <jake.edward.godsall@gmail.com>
2024-04-28 10:53:10 +0100

    updated Portfolio serializer to serialise many-to-many Skill using Skill.name attribute

--- backend/personal_site/portfolio/serializers.py
+++ backend/personal_site/portfolio/serializers.py
@@ -3,6 +3,11 @@ from .models import Portfolio
 
 
 class PortfolioSerializer(serializers.ModelSerializer):
+    skills = serializers.SerializerMethodField()
+
     class Meta:
         model = Portfolio
         fields = '__all__'
+
+    def get_skills(self, obj):
+        return [skill.name for skill in obj.skills.all()]

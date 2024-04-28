66719cfd2e6cfc3d2c7166a7fae61c2485c8a8e2
Jake Godsall <jake.edward.godsall@gmail.com>
2024-04-28 10:34:54 +0100

    added model serialiser and model viewset for the Portfolio model

--- backend/personal_site/portfolio/urls.py
+++ backend/personal_site/portfolio/urls.py
@@ -1,5 +1,8 @@
 from rest_framework import routers
 
+from .views import PortfolioViewSet
 
+router = routers.DefaultRouter()
+router.register("", PortfolioViewSet, basename="portfolio")
 
-urlpatterns = []
+urlpatterns = router.urls
--- backend/personal_site/portfolio/views.py
+++ backend/personal_site/portfolio/views.py
@@ -1,2 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
+from .models import Portfolio
+from .serializers import PortfolioSerializer
+class PortfolioViewSet(viewsets.ModelViewSet):
+    queryset = Portfolio.objects.all()
+    serializer_class = PortfolioSerializer

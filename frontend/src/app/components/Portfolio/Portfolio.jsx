47d9a3a3de47ab8494bcec6095458fc1519c2870
Jake Godsall <jake.edward.godsall@gmail.com>
2024-04-28 10:54:14 +0100

    modified the Portfolio component to fetch data from backend

--- frontend/src/app/components/Portfolio/Portfolio.jsx
+++ frontend/src/app/components/Portfolio/Portfolio.jsx
@@ -1,34 +1,34 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 import PortfolioItem from "./PortfolioItem";
 
-const DUMMY_DATA = [
-  {
-    id: 1,
-    title: "Fiskil",
-    image: "",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
-    skills: [
-      "React",
-      "Next.js",
-      "Typescript",
-      "Nest.js",
-      "PostgreSQL",
-      "TailwindCSS",
-      "Figma",
-      "Cypress",
-      "Git",
-    ],
-    liveSiteUrl: "a",
-    githubURL: "d",
-  },
-];
-
 export default function Portfolio() {
+  const [portfolioData, setPortfolioData] = useState([]);
+
+  useEffect(() => {
+    async function getPortfolioData(url) {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    }
+
+    async function fetchData() {
+      const data = await getPortfolioData("http://0.0.0.0:8000/api/portfolio");
+      setPortfolioData(data);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(portfolioData);
+
   return (
     <div>
       <h2 className="text-[2.4rem] font-semibold mb-[2.4rem]">My Work</h2>
       <ul className="flex flex-col items-center">
-        {DUMMY_DATA.map((portfolio) => (
+        {portfolioData.map((portfolio) => (
           <li key={portfolio.id} className="w-full">
             <PortfolioItem
               title={portfolio.title}

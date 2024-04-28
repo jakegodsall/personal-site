8f3afabb8f9828519aeba59ecf98c43d3ff78398
Jake Godsall <jake.edward.godsall@gmail.com>
2024-04-28 10:30:24 +0100

    updated the Portfolio model to allow for nullable urls

--- backend/personal_site/portfolio/models.py
+++ backend/personal_site/portfolio/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from daily_log.models import Skill
 
 
 class Portfolio(models.Model):
+    class Meta:
+        verbose_name = "Portfolio Detail"
+
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to="portfolio/thumbnails/")
-    live_url = models.URLField()
-    github_url = models.URLField()
+    image = models.ImageField(upload_to="portfolio/thumbnails/", )
+    live_url = models.URLField(blank=True, null=True)
+    github_url = models.URLField(blank=True, null=True)
+    skills = models.ManyToManyField(Skill)
 
     def __str__(self):
         return self.title

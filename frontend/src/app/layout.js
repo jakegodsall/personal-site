c49cf9ea8d10ff52c51296b7c9272db43132bb04
Jake Godsall <jake.edward.godsall@gmail.com>
2024-04-28 11:06:32 +0100

    added conditional favicon according to browser theme

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -8,6 +8,20 @@ const inter = Inter({ subsets: ["latin"], weights: ["400, 600, 700"] });
 export const metadata = {
   title: "Jake Godsall",
   description: "Personal website of Jake Godsall",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/assets/general/jg.svg",
+        href: "/assets/general/jg.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/assets/general/jg-darkmode.svg",
+        href: "/assets/general/jg-darkmode.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
